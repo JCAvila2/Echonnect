87c497d4e292e5d50803d310fa12ae50f898d9fd
Juan Camilo A <juankmilo1102@hotmail.com>
2024-08-30 00:02:37 -0500

    initial setup

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+
+<script lang="ts" >
+    export default {
+        setup() {
+            console.log('setup');
+        },
+        props: {
+            msg: String
+        }
+    }
+</script>
