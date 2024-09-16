0ed7ce0cd0c68365f9febf54117ffeaf7d0a7114
Juan Camilo A <juankmilo1102@hotmail.com>
2024-09-15 23:49:26 -0500

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,110 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <nav class="navbar">
+      <router-link to="/">
+        <div class="brand-title">
+          <img src="@/assets/logo.svg">
+            <h1>Echonnect</h1>
+        </div>
+      </router-link>
 
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <div class="navbar-links">
+        <ul>
+          <li> <router-link to="/search"> Search </router-link> </li>
+          <li> <router-link to="/profile"> Profile </router-link> </li>
+          <li> <router-link to="/upload"> Upload </router-link> </li>
+          <li> <router-link to="/settings"> Settings </router-link> </li>
+        </ul>
+      </div>
+    </nav>
+
   </header>
 
   <RouterView />
 </template>
 
+<!-- Non-scoped styles, global to the app -->
+<style>
+:root {
+  --header-height: 80px; /* Adjust according to the header height */
+}
+</style>
+
+<!-- Scoped styles for the component -->
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+  background-color: black;
+  padding: 0px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  z-index: 1000;
+  height: var(--header-height);
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* max-width: 1200px; */ /* Adjust according to the header width */
+  height: 100%;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.brand-title {
+  padding-left: 10px;
+  display: flex;
+  align-items: center;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.brand-title img {
+  height: 40px;
+  margin-right: 10px;
 }
 
-nav a:first-of-type {
-  border: 0;
+.brand-title h1 {
+  color: white;
+  font-size: 2.5rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.navbar-links ul {
+  list-style: none;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  height: var(--header-height);
+  align-items: center;
 }
+
+.navbar-links li {
+  height: 100%;
+}
+
+.navbar-links a {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 50px; /* Adjust padding to control the clickable area */
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  height: 100%;
+}
+
+/* TODO: change colors */
+.navbar-links a:hover {
+  color: black;
+  background-color: gray;
+}
+
+
+/* Margin to the top of the page content to avoid overlapping */
+body {
+  margin-top: var(--header-height);
+}
+
 </style>
