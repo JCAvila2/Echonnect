635064b8585a71850f7babc9ab9289e732d29b0f
JCAvila2 <juankmilo1102@hotmail.com>
2024-10-16 21:23:50 -0500

    navbar responsive design added

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -2,33 +2,47 @@
   <nav class="navbar">
     <router-link to="/">
       <div class="brand-title">
-        <img src="@/assets/blue-icon.svg">
+        <img src="@/assets/blue-icon.svg" alt="Logo">
         <h1>Echonnect</h1>
       </div>
     </router-link>
 
-    <div class="navbar-links">
+    <!-- Menu toggle for small screens -->
+    <div class="menu-toggle" @click="toggleMenu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <!-- Navbar links -->
+    <div :class="['navbar-links', { 'is-active': isMenuOpen }]">
       <ul>
-        <li> <router-link to="/search"> Search </router-link> </li>
-        <li v-if="uid"> <router-link to="/profile"> Profile </router-link> </li>
-        <li v-if="uid"> <router-link to="/upload"> Upload </router-link> </li>
-        <li v-if="uid"> <router-link to="/settings"> Settings </router-link> </li>
-        <li v-if="!uid"> <router-link to="/login"> Log In </router-link> </li>
-        <li v-if="!uid"> <router-link to="/register"> Register </router-link> </li>
+        <li> <router-link @click="toggleMenu" to="/search"> Search </router-link> </li>
+        <li v-if="uid"> <router-link @click="toggleMenu" to="/profile"> Profile </router-link> </li>
+        <li v-if="uid"> <router-link @click="toggleMenu" to="/upload"> Upload </router-link> </li>
+        <li v-if="uid"> <router-link @click="toggleMenu" to="/settings"> Settings </router-link> </li>
+        <li v-if="!uid"> <router-link @click="toggleMenu" to="/login"> Log In </router-link> </li>
+        <li v-if="!uid"> <router-link @click="toggleMenu" to="/register"> Register </router-link> </li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
 import { useAuthStore } from '@/stores/auth';
 
 export default {
   setup() {
     const authStore = useAuthStore();
     const uid = computed(() => authStore.user?.uid);
-    return { uid };
+    const isMenuOpen = ref(false);
+
+    const toggleMenu = () => {
+      isMenuOpen.value = !isMenuOpen.value;
+    };
+
+    return { uid, isMenuOpen, toggleMenu };
   },
 };
 </script>
@@ -108,5 +122,71 @@ header {
 .navbar-links a.router-link-active {
   color: black;
   background-color: gray;
+}
+
+/* Menu toggle (hamburger icon) for mobile */
+.menu-toggle {
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  margin-right: 20px;
+}
+
+.menu-toggle span {
+  width: 30px;
+  height: 3px;
+  background-color: white;
+  margin: 5px 0;
+}
+
+/* Media query for smaller screens */
+@media (max-width: 768px) {
+  .navbar {
+    padding: 0 10px;
+  }
+
+  .navbar-links {
+    display: none;
+    position: absolute;
+    top: var(--header-height);
+    right: 0;
+    background-color: black;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .navbar-links.is-active {
+    display: flex;
+  }
+
+  .navbar-links ul {
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .navbar-links li {
+    width: 100%;
+    text-align: center;
+    margin: 0;
+    background-color: black;
+  }
+
+  .navbar-links a {
+    width: 100%;
+    padding: 15px 0;
+  }
+
+  .navbar-links a.router-link-active {
+    background-color: gray;
+  }
+
+  .menu-toggle {
+    display: flex;
+  }
+
+  .brand-title h1 {
+    font-size: 2rem;
+  }
 }
 </style>
