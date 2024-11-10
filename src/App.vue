d21fccb86d9a600e9b6a38c9f80a3bff96fb5a44
JCAvila2 <juankmilo1102@hotmail.com>
2024-11-10 03:10:07 -0500

    check for user settings on firebase or set defaults

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { defineComponent } from 'vue';
 import Navbar from '@/components/NavbarComponent.vue';
 import { RouterView } from 'vue-router';
 import { useThemeStore } from '@/stores/theme';
+import { useI18n } from 'vue-i18n';
+import { useAuthStore } from '@/stores/auth';
+import { doc, collection, getDoc, updateDoc } from 'firebase/firestore';
+import { db } from '@/firebase';
 
 export default defineComponent({
   components: {
@@ -11,19 +15,73 @@ export default defineComponent({
   },
   setup() {
     document.title = 'Echonnect';
-
+    const authStore = useAuthStore();
     const themeStore = useThemeStore();
-    themeStore.setTheme('dark');
-
+    const { t, locale } = useI18n();
+    
     return {
       themeStore,
+      authStore,
+      t, 
+      locale,
     };
   },
+  computed: {
+    uid() {
+      return this.authStore.user?.uid;
+    }, 
+    language: {
+      get() {
+        return this.locale;
+      },
+      set(lang: string) {
+        this.$i18n.locale = lang;
+      },
+    },
+  },
   watch: {
+    uid: {
+      async handler(newUid) {
+        if (newUid) {
+          const userDoc = doc(collection(db, 'users'), newUid);
+          const docSnapshot = await getDoc(userDoc);
+          if (docSnapshot.exists()) {
+            const userTheme = docSnapshot.data().theme;
+            const userLang = docSnapshot.data().language;
+            this.themeStore.setTheme(userTheme ?? 'dark');
+            this.language = userLang ?? 'en';
+          } else {
+            this.themeStore.setTheme(this.themeStore.theme ?? 'dark');
+          }
+        } else {
+          this.themeStore.setTheme(this.themeStore.theme ?? 'dark');
+          this.language = this.language ?? 'en';
+        }
+      },
+      immediate: true,
+    },
+    language: {
+      async handler(newLang) {
+        this.$i18n.locale = newLang;
+        if (this.uid) {
+          const userDoc = doc(collection(db, 'users'), this.uid);
+          await updateDoc(userDoc, {
+            language: newLang,
+          });
+        }
+      },
+      immediate: true,
+    },
     themeStore: {
       deep: true,
       handler() {
         document.body.className = this.themeStore.theme;
+        if (this.uid) {
+          const userDoc = doc(collection(db, 'users'), this.uid);
+          updateDoc(userDoc, {
+            theme: this.themeStore.theme,
+          });
+        }
       },
     },
   },
